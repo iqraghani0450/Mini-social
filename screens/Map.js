a88ab5cfd4b8069d542ed150c4c85e591f357251
iqraghani0450 <iqraghani0450@gmail.com>
2022-12-09 15:48:08 +0500

    updated maps

--- screens/Map.js
+++ screens/Map.js
@@ -3,12 +3,20 @@ import React, { useEffect, useRef, useState } from 'react'
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 import Geolocation from 'react-native-geolocation-service'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import { FlatList } from 'react-native-gesture-handler'
 
 const Map = () => {
 
   const [long, setLong] = useState(0)
   const [lat, setLat] = useState(0)
   const back = useRef()
+  const locations = [
+    { latitude: 25.40452886225312, longitude: 68.32728105700944 },
+    { latitude: 25.412348853646748, longitude: 68.33802622180428 },
+    { latitude: 25.395283962171934, longitude: 68.33278545714347 },
+    { latitude: 25.38830543776189, longitude: 68.34154018686651 },
+    { latitude: 25.423814174749356, longitude: 68.34132561169426 }
+  ]
 
   useEffect(() => {
 
@@ -31,12 +39,14 @@ const Map = () => {
     }
   }
   const MoveToLocation = () => {
-    back.current.animateToRegion({ 
-      latitude: lat, 
+    back.current.animateToRegion({
+      latitude: lat,
       longitude: long,
       latitudeDelta: 0.015,
-      longitudeDelta: 0.015 })
+      longitudeDelta: 0.015
+    })
   }
+
 
   return (
     <View style={styles.container}>
@@ -54,11 +64,25 @@ const Map = () => {
       >
         <Marker
           coordinate={{ latitude: lat, longitude: long }}
+          style={{zIndex: 1}}
         >
           <Icon name={"map-pin"} size={35} color={"#2D4874"} />
         </Marker>
+        
+        {locations.map((item, index) => {
+          return (
+            <Marker
+              key={index}
+              coordinate={{ latitude: item.latitude, longitude: item.longitude }}
+            >
+              <Icon name={"map-signs"} size={35} color={"red"} />
+
+            </Marker>
+          )
+        })}
+
       </MapView>
-      <TouchableOpacity style={{alignSelf: 'flex-end', bottom: 40, right: 20, backgroundColor: "white", borderRadius: 60 }} onPress={() => { MoveToLocation() }}>
+      <TouchableOpacity style={{ alignSelf: 'flex-end', bottom: 40, right: 20, backgroundColor: "white", borderRadius: 60 }} onPress={() => { MoveToLocation() }}>
         <Icon name={"safari"} size={40} color={"#2D4874"} />
       </TouchableOpacity>
     </View>
